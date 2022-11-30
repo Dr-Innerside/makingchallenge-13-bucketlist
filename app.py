6ec3cd9bfaaf279f3c6b680147cd2c7884c7f501
migdracios <migdracios@gmail.com>
2022-11-30 19:40:54 +0900

    add login basic logic

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 import os
 
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import create_access_token
+
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
@@ -11,6 +16,11 @@ load_dotenv()
 URL = os.environ.get('client')
 client = MongoClient(URL)
 db = client.makingchallenge
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.environ.get('SECRET_KEY')
+jwt = JWTManager(app)
+
 
 @app.route('/')
 def show_index():
@@ -20,15 +30,25 @@ def show_index():
 def signup():
     username_receive = request.form['username']
     password_receive = bcrypt.generate_password_hash(request.form['password'])
-
     doc = {
         'username': username_receive,
         'password': password_receive
     }
     db.user.insert_one(doc)
-    
+    return jsonify({"msg": "회원가입에 성공했습니다!"}), 200
 
-    return jsonify({"msg": "회원가입에 성공했습니다!"})
+@app.route('/bucket/login', methods=['POST'])
+def login():
+    username_receive = request.form['username']
+    password_receive = request.form['password']
+    password_data = db.user.find_one({'username':username_receive},{'_id':False})['password']
+    is_password_correct = bcrypt.check_password_hash(password_data, password_receive)
+    if not is_password_correct:
+        return jsonify({'msg': '아이디, 비밀번호가 다릅니다'}), 401
+    
+    access_token = create_access_token(identity=username_receive)
+    return jsonify({'msg':f'{username_receive}님 환영합니다', 'access_token': access_token}), 200
+    
 
 if __name__ == "__main__":
     app.run('0.0.0.0',port=8080, debug=True)
